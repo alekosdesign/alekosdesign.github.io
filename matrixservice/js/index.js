6da99033f135758e2ab62b491d84abf96a978a38
AlEkos Design <38691899+alekosdesigne@users.noreply.github.com>
2018-05-07 16:41:23 +1000

    0.3

--- matrixservice/js/index.js
+++ matrixservice/js/index.js
@@ -4,7 +4,7 @@ $( () => {
 	$(window).scroll( () => {
 		var windowTop = $(window).scrollTop();
 		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
-		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','80px');
 	});
 	
 	//Click Logo To Scroll To Top
@@ -26,11 +26,35 @@ $( () => {
 	$('#menu-toggle').on('click', () => {
 		$('#menu-toggle').toggleClass('closeMenu');
 		$('ul').toggleClass('showMenu');
-		
 		$('li').on('click', () => {
 			$('ul').removeClass('showMenu');
 			$('#menu-toggle').removeClass('closeMenu');
 		});
+		$('section__blur').toggleClass('blur');
 	});
 	
 });
+
+// time play
+function getDate()
+{
+	var date = new Date();
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var seconds = date.getSeconds();
+	//По надобности условие ниже повторить с minutes и hours
+	if(minutes < 10)
+	{
+		minutes = '0' + minutes;
+	}
+	if(seconds < 10)
+	{
+		seconds = '0' + seconds;
+	}
+	if(hours < 10)
+	{
+		hours = '0' + hours;
+	}
+	document.getElementById('timedisplay').innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+setInterval(getDate, 0);
