3db54063ce8023b8ebe08c15189c43b04f153ff2
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2018-11-12 16:46:24 +1000

    mobilemenu

--- 10min/main.js
+++ 10min/main.js
@@ -1,12 +1,28 @@
 $(window).scroll(function() {
-var height = $(window).scrollTop();
-     /*Если сделали скролл на 100px задаём новый класс для header*/
-if(height > 150){
+	var height = $(window).scrollTop();
+	/*Если сделали скролл на 100px задаём новый класс для header*/
+	if(height > 250){
 
-$('nav').addClass('addnav');
-} else{
-     /*Если меньше 100px удаляем класс для header*/
-$('nav').removeClass('addnav');
-}
+		$('.mainmenu').addClass('addnav');
+	} else{
+		/*Если меньше 100px удаляем класс для header*/
+		$('.mainmenu').removeClass('addnav');
+	}
 
 });
+
+$(document).ready(function() {
+	var $page = $('html, body');
+	$('a[href*="#"]').click(function() {
+		$page.animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 1000);
+		return false;
+	});
+});
+
+
+function openMenu() {
+	document.getElementById('sidebar').classList.toggle('active')
+	document.getElementById('id-toggle-btn').classList.toggle('active-btn')
+}
