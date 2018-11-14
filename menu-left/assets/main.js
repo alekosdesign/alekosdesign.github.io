276c7b3bfc0f057ebc9b43a741ce1de03c256078
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2018-11-14 19:18:27 +1000

    media

--- menu-left/assets/main.js
+++ menu-left/assets/main.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 			$("#menuicon").animate({right:'-100'},300);
 		}
 		$('#bg-image').addClass('blur');
+		$('section').addClass('blur');
 		setTimeout(showMenu,100);
 	});
 
@@ -13,9 +14,11 @@ $(document).ready(function(){
 		$("#mainmenu").css("-webkit-clip-path","polygon(0 0,0% 0,100% 100%,0% 100%)");
 		function hideMenu(){
 			$("#mainmenu").css("left","-300px");
-			$("#menuicon").animate({right:'50'},300);
+			$("#menuicon").animate({right:'40'},300);
 		}
 		$('#bg-image').removeClass('blur');
+		$('section').removeClass('blur');
+
 		setTimeout(hideMenu,300);
 
 		function originalLayout(){
@@ -23,4 +26,39 @@ $(document).ready(function(){
 		}
 		setTimeout(originalLayout,600);
 	});
+	$("section").click(function(){
+		$("#mainmenu").css("-webkit-clip-path","polygon(0 0,0% 0,100% 100%,0% 100%)");
+		function hideMenu(){
+			$("#mainmenu").css("left","-300px");
+			$("#menuicon").animate({right:'40'},300);
+		}
+		$('#bg-image').removeClass('blur');
+		$('section').removeClass('blur');
+
+		setTimeout(hideMenu,300);
+
+		function originalLayout(){
+			$("#mainmenu").css("-webkit-clip-path","polygon(0 0,100% 0,0% 100%,0% 100%)");
+		}
+		setTimeout(originalLayout,600);
+	});
+	$("#bg-image").click(function(){
+		$("#mainmenu").css("-webkit-clip-path","polygon(0 0,0% 0,100% 100%,0% 100%)");
+		function hideMenu(){
+			$("#mainmenu").css("left","-300px");
+			$("#menuicon").animate({right:'40'},300);
+		}
+		$('#bg-image').removeClass('blur');
+		$('section').removeClass('blur');
+
+		setTimeout(hideMenu,300);
+
+		function originalLayout(){
+			$("#mainmenu").css("-webkit-clip-path","polygon(0 0,100% 0,0% 100%,0% 100%)");
+		}
+		setTimeout(originalLayout,600);
+	});
+
 });
+
+
