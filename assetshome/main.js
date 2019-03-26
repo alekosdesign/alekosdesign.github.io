e96cc172c0b15c0b4dd0a6e95a013fbc83dc788d
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2019-03-26 14:51:19 +1000

    v5

--- assetshome/main.js
+++ assetshome/main.js
@@ -1,16 +1,26 @@
-$(document).ready(function() {
-	var rellax = new Rellax('.rellax');
-	var $page = $('html, body');
-	$('a[href*="#"]').click(function() {
-		$page.animate({
-			scrollTop: $($.attr(this, 'href')).offset().top
-		}, 1000);
-		return false;
-	});
+// $(document).ready(function() {
+// 	var rellax = new Rellax('.rellax');
+// 	var $page = $('html, body');
+// 	$('a[href*="#"]').click(function() {
+// 		$page.animate({
+// 			scrollTop: $($.attr(this, 'href')).offset().top
+// 		}, 1000);
+// 		return false;
+// 	});
+// });
+// $(document).ready(function() {
+// 	$("#openmenu").click(function(){
+// 		$("#main").fadeIn(200);
+// 		$("#openmenu").animate({left:"-10%"})
+// 	})
+// });
+
+
+// why it doesn't work on firefox?
+var card = $(".card");
+
+$(document).on("mousemove",function(e) {  
+  var ax = -($(window).innerWidth()/2- e.pageX)/20;
+  var ay = ($(window).innerHeight()/2- e.pageY)/10;
+  card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
 });
-$(document).ready(function() {
-	$("#openmenu").click(function(){
-		$("#main").fadeIn(200);
-		$("#openmenu").animate({left:"-10%"})
-	})
-})
