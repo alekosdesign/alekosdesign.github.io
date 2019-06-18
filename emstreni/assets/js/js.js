ae9a66ec267c23eb1df6e764add885fc7032b876
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2019-06-19 05:08:11 +1000

    v2

--- emstreni/assets/js/js.js
+++ emstreni/assets/js/js.js
@@ -7,37 +7,37 @@
 // 		} 
 // 	}, 500); 
 // };
-$('.icon__menu').on('click', function(e) {
-	e.preventDefault;
-	$(this).toggleClass('icon__menu__active');
-});
-$(document).ready(function(){
-	$('.icon__menu').click(function(){
-		$('.mainmenu').slideToggle(300);
-	});
-});
-// открыть по кнопке
-$('.js__modal__form').click(function() { 
-	$('.js__overlay__form').fadeIn();
-	$('.js__overlay__form').addClass('disabled');
-});
-$('.js__modal__page').click(function() { 
-	$('.js__overlay__page').fadeIn();
-	$('.js__overlay__page').addClass('disabled');
-});
-// закрыть на крестик
-$('.js__modal__close__form').click(function() { 
-	$('.js__overlay__form').fadeOut();
-});
-$('.js__modal__close__page').click(function() { 
-	$('.js__overlay__page').fadeOut();
-});
-$(document).ready(function() {
-	var $page = $('html, body');
-	$('a[href*="#"]').click(function() {
-		$page.animate({
-			scrollTop: $($.attr(this, 'href')).offset().top
-		}, 1200);
-		return false;
-	});
-});
+// $('.icon__menu').on('click', function(e) {
+// 	e.preventDefault;
+// 	$(this).toggleClass('icon__menu__active');
+// });
+// $(document).ready(function(){
+// 	$('.icon__menu').click(function(){
+// 		$('.mainmenu').slideToggle(300);
+// 	});
+// });
+// // открыть по кнопке
+// $('.js__modal__form').click(function() { 
+// 	$('.js__overlay__form').fadeIn();
+// 	$('.js__overlay__form').addClass('disabled');
+// });
+// $('.js__modal__page').click(function() { 
+// 	$('.js__overlay__page').fadeIn();
+// 	$('.js__overlay__page').addClass('disabled');
+// });
+// // закрыть на крестик
+// $('.js__modal__close__form').click(function() { 
+// 	$('.js__overlay__form').fadeOut();
+// });
+// $('.js__modal__close__page').click(function() { 
+// 	$('.js__overlay__page').fadeOut();
+// });
+// $(document).ready(function() {
+// 	var $page = $('html, body');
+// 	$('a[href*="#"]').click(function() {
+// 		$page.animate({
+// 			scrollTop: $($.attr(this, 'href')).offset().top
+// 		}, 1200);
+// 		return false;
+// 	});
+// });
