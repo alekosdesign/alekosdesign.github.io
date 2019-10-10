747ac03d786a0d189c399dd9fbcd9ec3a24ce7a1
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2019-10-10 17:12:41 +1000

    1

--- assetshome/main.js
+++ assetshome/main.js
@@ -1,22 +1,3 @@
-// $(document).ready(function() {
-// 	var rellax = new Rellax('.rellax');
-// 	var $page = $('html, body');
-// 	$('a[href*="#"]').click(function() {
-// 		$page.animate({
-// 			scrollTop: $($.attr(this, 'href')).offset().top
-// 		}, 1000);
-// 		return false;
-// 	});
-// });
-// $(document).ready(function() {
-// 	$("#openmenu").click(function(){
-// 		$("#main").fadeIn(200);
-// 		$("#openmenu").animate({left:"-10%"})
-// 	})
-// });
-
-
-// why it doesn't work on firefox?
 var card = $(".card");
 
 $(document).on("mousemove",function(e) {  
@@ -24,3 +5,4 @@ $(document).on("mousemove",function(e) {
   var ay = ($(window).innerHeight()/2- e.pageY)/10;
   card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
 });
+
