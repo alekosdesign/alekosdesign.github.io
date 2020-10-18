a867eaa806994809fbb1a9f1fb24b90c30d4854f
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2020-10-19 02:29:16 +1000

    cyberpunk

--- assets/main.js
+++ assets/main.js
@@ -23,3 +23,31 @@ $(document).ready(function() {
 // 	} 
 // 	return myMessage; 
 // };
+
+
+
+$(document).ready(function() {
+	// var tl = new gsap();
+	var tl = gsap;
+
+	var logo = $(".hero .logo__white,.hero .logo__black")
+	var herotext = $(".hero__h1__eng, .hero__h1__ru")
+
+	// tl.fromTo(logo, 2, {scale:0},{scale:1, easy: Power2.easyOut});
+	// tl.fromTo(herotext, 1.2, {x:"-100%"},{x:"0%", ease:Power2.easyInOut},"-=1");
+	// tl.from(herotext, 1.2, {opacity: 0, x: -200, color: "red", easy: Power2.easyInOut});
+	// tl.from (logo, 1, {
+	// 	delay: 0.2,
+	// 	y: 20,
+	// 	opacity: 0,
+	// 	ease:Expo.easeInOut
+	// });
+	tl.from(logo, 1, {
+		delay: 0.4,
+		y: 20,
+		opacity: 0,
+		ease:Expo.easeInOut
+	});
+});
+
+// ease:Back.easeOut
