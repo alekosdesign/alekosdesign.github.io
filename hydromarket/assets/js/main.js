0a8d79df0c2e0b266c2aec68646903c36d9fad17
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2021-08-31 15:42:19 +1000

    shlif

--- hydromarket/assets/js/main.js
+++ hydromarket/assets/js/main.js
@@ -3,9 +3,11 @@ document.body.onload = function() {
 		var preloader = document.getElementById("preloader");
 		preloader.classList.contains("done") || preloader.classList.add("done");
 	}, 500);
-}
-var h = $('header').height();
-$('.hero__slider').css({'padding-top': h});
+};
+$(document).ready(function() {
+	var h = $('header').height();
+	$('.title, .hero__molot').css({'padding-top': h});
+});
 $('.offer').slick({
 	infinite: true,
 	swipeToSlide: true,
@@ -33,22 +35,80 @@ $('.hero__slider').slick({
 	slidesToScroll: 1,
 	centerPadding: '40px',
 });
-// lightbox.option({
-// 	'resizeDuration': 200,
-// 	'wrapAround': true
-//   })
-// $(document).ready(function() {
-//     $("body").on("click", "#modal", function() {
-//         $(".modal").fadeIn(300),
-//         $(".modal__overlay").fadeIn(300),
-//         $("body").css("overflow", "hidden")
-//     }),
-//     $(".cross, .modal__overlay").click(function() {
-//         $(".modal").fadeOut(300),
-//         $(".modal__overlay").fadeOut(300),
-//         $("body").css("overflow", "auto")
-//     })
-// }),
+$(document).ready(function() {
+	$("body").on("click", "#open__modal__feedback", function() {
+		$("#modal__feedback").fadeIn(300),
+		$("body").css("overflow", "hidden")
+	}),
+	$(".modal__feedback__close").click(function() {
+		$("#modal__feedback").fadeOut(300),
+		$("body").css("overflow", "auto")
+	});
+
+	$("body").on("click", ".header__profile__login", function() {
+		$("#modal__reg").fadeIn(300),
+		$("body").css("overflow", "hidden")
+	}),
+	$(".modal__feedback__close").click(function() {
+		$("#modal__reg").fadeOut(300),
+		$("body").css("overflow", "auto")
+	});
+	
+	$("body").on("click", "#open__modal__login", function() {
+		$("#modal__login").fadeIn(300),
+		$("#modal__reg").fadeOut(300),
+		$("body").css("overflow", "hidden")
+	}),
+	$(".modal__feedback__close").click(function() {
+		$("#modal__reg").fadeOut(300),
+		$("body").css("overflow", "auto")
+	});
+
+	$("body").on("click", "#open__modal__reg", function() {
+		$("#modal__reg").fadeIn(300),
+		$("#modal__login").fadeOut(300),
+		$("body").css("overflow", "hidden")
+	}),
+	$(".modal__feedback__close").click(function() {
+		$("#modal__login").fadeOut(300),
+		$("body").css("overflow", "auto")
+	});
+});
+
 $(window).scroll(function() {
-	150 < $(window).scrollTop() ? $("header").addClass("scroll") : $("header").removeClass("scroll")
-})
+	30 < $(window).scrollTop() ? $("header").addClass("scroll") : $("header").removeClass("scroll")
+});
+// Mask Phone JS
+window.addEventListener("DOMContentLoaded", function() {
+	var keyCode;
+	
+	function mask(event) {
+	event.keyCode && (keyCode = event.keyCode);
+	var pos = this.selectionStart;
+	if (pos < 1) event.preventDefault();
+	var matrix = "+7 (___) ___-__-__",
+	i = 0,
+	def = matrix.replace(/\D/g, ""),
+	val = this.value.replace(/\D/g, ""),
+	new_value = matrix.replace(/[_\d]/g, function(a) {
+	return i < val.length? val.charAt(i++) || def.charAt(i): a
+	});
+	i = new_value.indexOf("_");
+	if (i != -1) {
+	i < 3 && (i = 1);
+	new_value = new_value.slice(0, i)
+	}
+	var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+	function(a) {
+	return "\\d{1," + a.length + "}"
+	}).replace(/[+()]/g, "\\$&");
+	reg = new RegExp("^" + reg + "$");
+	if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+	if (event.type == "blur" && this.value.length < 5) this.value = ""
+	}
+	var input = document.querySelector("#tel");
+	input.addEventListener("input", mask, false);
+	input.addEventListener("focus", mask, false);
+	input.addEventListener("blur", mask, false);
+	input.addEventListener("keydown", mask, false)
+});
