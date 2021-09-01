c99d2a7c380fb2e35a0f2120d4772ece8b9f1c5d
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2021-09-01 17:40:19 +1000

    shlif

--- hydromarket/assets/js/main.js
+++ hydromarket/assets/js/main.js
@@ -1,13 +1,31 @@
-document.body.onload = function() {
-	setTimeout(function() {
-		var preloader = document.getElementById("preloader");
-		preloader.classList.contains("done") || preloader.classList.add("done");
-	}, 500);
-};
+// document.body.onload = function() {
+// 	setTimeout(function() {
+// 		var preloader = document.getElementById("preloader");
+// 		preloader.classList.contains("done") || preloader.classList.add("done");
+// 	}, 500);
+// };
 $(document).ready(function() {
-	var h = $('header').height();
+	var width = $(window).width();
+	if (768 >= width){
+		$(".contacts__item:last-child, #gallary, #fits").detach();
+	}
+});
+$(document).ready(function() {
+	var h = $('header').outerHeight();
 	$('.title, .hero__molot').css({'padding-top': h});
 });
+$(window).scroll(function() {
+	var height = $(window).scrollTop();
+	if (height > 150) {
+		$('header').addClass('scroll'),
+		$('.scroll .header__top>*').fadeOut(400)
+	} else {
+		$('header').removeClass('scroll'),
+		$('.header__top>*').fadeIn(400)
+	}
+});
+
+
 $('.offer').slick({
 	infinite: true,
 	swipeToSlide: true,
@@ -16,6 +34,46 @@ $('.offer').slick({
 	slidesToShow: 4,
 	slidesToScroll: 1,
 	centerPadding: '40px',
+	autoplay: true,
+	autoplaySpeed: 10000,
+	lazyLoad: 'ondemand',
+	responsive: [
+		{
+		  breakpoint: 1025,
+		  settings: {
+			centerMode: true,
+			centerPadding: '40px',
+			slidesToShow: 3
+		  }
+		},
+		{
+		  breakpoint: 768,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '20px',
+			slidesToShow: 3
+		  }
+		},
+		{
+		  breakpoint: 561,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '20px',
+			slidesToShow: 2
+		  }
+		},
+		{
+		  breakpoint: 425,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '10px',
+			slidesToShow: 1
+		  }
+		}
+	  ]
 });
 $('.partners').slick({
 	infinite: true,
@@ -25,6 +83,46 @@ $('.partners').slick({
 	slidesToShow: 4,
 	slidesToScroll: 1,
 	centerPadding: '40px',
+	autoplay: true,
+	autoplaySpeed: 10000,
+	lazyLoad: 'ondemand',
+	responsive: [
+		{
+		  breakpoint: 1025,
+		  settings: {
+			centerMode: true,
+			centerPadding: '40px',
+			slidesToShow: 3
+		  }
+		},
+		{
+		  breakpoint: 768,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '20px',
+			slidesToShow: 3
+		  }
+		},
+		{
+		  breakpoint: 561,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '20px',
+			slidesToShow: 2
+		  }
+		},
+		{
+		  breakpoint: 425,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '10px',
+			slidesToShow: 1
+		  }
+		}
+	  ]
 });
 $('.hero__slider').slick({
 	infinite: true,
@@ -34,50 +132,79 @@ $('.hero__slider').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	centerPadding: '40px',
+	autoplay: true,
+	autoplaySpeed: 15000,
+	lazyLoad: 'ondemand',
 });
 $(document).ready(function() {
+	// Открытие модальных окон
+	// Обратная связь
 	$("body").on("click", "#open__modal__feedback", function() {
 		$("#modal__feedback").fadeIn(300),
 		$("body").css("overflow", "hidden")
 	}),
-	$(".modal__feedback__close").click(function() {
-		$("#modal__feedback").fadeOut(300),
-		$("body").css("overflow", "auto")
-	});
-
-	$("body").on("click", ".header__profile__login", function() {
+	// ВходРегистрация
+	$("body").on("click", "#registration", function() {
 		$("#modal__reg").fadeIn(300),
 		$("body").css("overflow", "hidden")
 	}),
-	$(".modal__feedback__close").click(function() {
-		$("#modal__reg").fadeOut(300),
-		$("body").css("overflow", "auto")
-	});
-	
+	// Вход
+	$("body").on("click", "#login", function() {
+		$("#modal__reg").fadeIn(300),
+		$("body").css("overflow", "hidden")
+	}),
+
+	// Переход с регистации на логин и обратно
 	$("body").on("click", "#open__modal__login", function() {
 		$("#modal__login").fadeIn(300),
 		$("#modal__reg").fadeOut(300),
 		$("body").css("overflow", "hidden")
 	}),
-	$(".modal__feedback__close").click(function() {
-		$("#modal__reg").fadeOut(300),
-		$("body").css("overflow", "auto")
-	});
-
 	$("body").on("click", "#open__modal__reg", function() {
 		$("#modal__reg").fadeIn(300),
 		$("#modal__login").fadeOut(300),
 		$("body").css("overflow", "hidden")
+	}),
+
+
+	// Закрытие модальных окон
+	$(".modal__feedback__close").click(function() {
+		$("#modal__feedback").fadeOut(300),
+		$("body").css("overflow", "auto")
+	}),
+	// $("body").on("click", ".header__profile__login", function() {
+	// 	$("#modal__reg").fadeIn(300),
+	// 	$("body").css("overflow", "hidden")
+	// }),
+	$(".modal__feedback__close").click(function() {
+		$("#modal__reg").fadeOut(300),
+		$("body").css("overflow", "auto")
+	}),
+	$(".modal__feedback__close").click(function() {
+		$("#modal__reg").fadeOut(300),
+		$("body").css("overflow", "auto")
 	}),
 	$(".modal__feedback__close").click(function() {
 		$("#modal__login").fadeOut(300),
 		$("body").css("overflow", "auto")
 	});
 });
+var login = $('.header__profile__login');
+login.hover(function(){
+	$('.but__personal').addClass('active__but__personal')
+}),
+login.mouseleave(function(){
+	setTimeout(function(){
+		$('.but__personal').removeClass('active__but__personal')
+	}, 1000)
+}),
+login.click(function(){
+	$('.but__personal').toggleClass('active__but__personal')
+}),
+$('.but__personal span').click(function(){
+	$('.but__personal').removeClass('active__but__personal')
+}),
 
-$(window).scroll(function() {
-	30 < $(window).scrollTop() ? $("header").addClass("scroll") : $("header").removeClass("scroll")
-});
 // Mask Phone JS
 window.addEventListener("DOMContentLoaded", function() {
 	var keyCode;
