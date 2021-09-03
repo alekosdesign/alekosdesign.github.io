4d6bb0241c018cb0153d7ff75edd56eb320c63f7
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2021-09-03 17:49:22 +1000

    adsa

--- hydromarket/assets/js/main.js
+++ hydromarket/assets/js/main.js
@@ -1,40 +1,40 @@
-document.body.onload = function() {
-	setTimeout(function() {
+document.body.onload = function () {
+	setTimeout(function () {
 		$('#preloader').fadeOut(700);
 		// $('body').css({'overflow':'auto'})
 	}, 700);
-	setTimeout(function() {
+	setTimeout(function () {
 		$('#preloader::after').fadeOut(700)
 	}, 0);
 },
-$(document).ready(function() {
-	var width = $(window).width();
-	if (768 >= width){
-		$(".contacts__item:last-child, #gallary, #fits").detach();
-	}
-});
-$(document).ready(function() {
+	$(document).ready(function () {
+		var width = $(window).width();
+		if (768 >= width) {
+			$(".contacts__item:last-child, #gallary, #fits").detach();
+		}
+	});
+$(document).ready(function () {
 	var h = $('header').outerHeight();
-	$('.title, .hero__molot, #margintop').css({'padding-top': h});
+	$('.title, .hero__molot, #margintop').css({ 'padding-top': h });
 });
-$(document).ready(function() {
+$(document).ready(function () {
 	var windowWidth = $(window).width();
-	$('.error').mousemove(function(event) {
+	$('.error').mousemove(function (event) {
 		var moveX = (($(window).width() / 2) - event.pageX) * 0.02;
 		var moveY = (($(window).height() / 2) - event.pageY) * 0.02;
 		$('.error__404 svg').css('transform', 'translate(' + moveX + 'px,' + moveY + 'px)'),
-		$('.error__404 h4, .error__404 p, .error__404 a').css('transform', 'translate(' + moveX + 'px,' + moveY + 'px)'),
-		$('.error::after').css('transform', 'translate(' + moveX + 'px,0px)')
+			$('.error__404 h4, .error__404 p, .error__404 a').css('transform', 'translate(' + moveX + 'px,' + moveY + 'px)'),
+			$('.error::after').css('transform', 'translate(' + moveX + 'px,0px)')
 	})
 });
-$(window).scroll(function() {
+$(window).scroll(function () {
 	var height = $(window).scrollTop();
 	if (height > 150) {
 		$('header').addClass('scroll'),
-		$('.scroll .header__top').fadeOut(400)
+			$('.scroll .header__top').fadeOut(400)
 	} else {
 		$('header').removeClass('scroll'),
-		$('.header__top').fadeIn(400)
+			$('.header__top').fadeIn(400)
 	}
 });
 
@@ -52,41 +52,41 @@ $('.offer').slick({
 	lazyLoad: 'ondemand',
 	responsive: [
 		{
-		  breakpoint: 1367,
-		  settings: {
-			centerMode: true,
-			centerPadding: '30px',
-			slidesToShow: 3
-		  }
+			breakpoint: 1367,
+			settings: {
+				centerMode: true,
+				centerPadding: '30px',
+				slidesToShow: 3
+			}
 		},
 		{
-		  breakpoint: 768,
-		  settings: {
-			arrows: false,
-			centerMode: true,
-			centerPadding: '20px',
-			slidesToShow: 3
-		  }
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '20px',
+				slidesToShow: 3
+			}
 		},
 		{
-		  breakpoint: 561,
-		  settings: {
-			arrows: false,
-			centerMode: true,
-			centerPadding: '20px',
-			slidesToShow: 2
-		  }
+			breakpoint: 561,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '20px',
+				slidesToShow: 2
+			}
 		},
 		{
-		  breakpoint: 425,
-		  settings: {
-			arrows: false,
-			centerMode: true,
-			centerPadding: '10px',
-			slidesToShow: 1
-		  }
+			breakpoint: 425,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '10px',
+				slidesToShow: 1
+			}
 		}
-	  ]
+	]
 });
 $('.partners').slick({
 	infinite: true,
@@ -101,41 +101,41 @@ $('.partners').slick({
 	lazyLoad: 'ondemand',
 	responsive: [
 		{
-		  breakpoint: 1367,
-		  settings: {
-			centerMode: true,
-			centerPadding: '30px',
-			slidesToShow: 3
-		  }
+			breakpoint: 1367,
+			settings: {
+				centerMode: true,
+				centerPadding: '30px',
+				slidesToShow: 3
+			}
 		},
 		{
-		  breakpoint: 768,
-		  settings: {
-			arrows: false,
-			centerMode: true,
-			centerPadding: '20px',
-			slidesToShow: 3
-		  }
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '20px',
+				slidesToShow: 3
+			}
 		},
 		{
-		  breakpoint: 561,
-		  settings: {
-			arrows: false,
-			centerMode: true,
-			centerPadding: '20px',
-			slidesToShow: 2
-		  }
+			breakpoint: 561,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '20px',
+				slidesToShow: 2
+			}
 		},
 		{
-		  breakpoint: 425,
-		  settings: {
-			arrows: false,
-			centerMode: true,
-			centerPadding: '10px',
-			slidesToShow: 1
-		  }
+			breakpoint: 425,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '10px',
+				slidesToShow: 1
+			}
 		}
-	  ]
+	]
 });
 $('.hero__slider').slick({
 	infinite: true,
@@ -149,114 +149,114 @@ $('.hero__slider').slick({
 	autoplaySpeed: 15000,
 	lazyLoad: 'ondemand',
 });
-$(document).ready(function() {
+$(document).ready(function () {
 	// Открытие модальных окон
 	// Обратная связь
-	$("body").on("click", "#open__modal__feedback", function() {
+	$("body").on("click", "#open__modal__feedback", function () {
 		$("#modal__feedback").fadeIn(300),
-		$("body").css("overflow", "hidden")
+			$("body").css("overflow", "hidden")
 	}),
-	// ВходРегистрация
-	$("body").on("click", "#registration", function() {
-		$("#modal__reg").fadeIn(300),
-		$("body").css("overflow", "hidden")
-	}),
-	// Вход
-	$("body").on("click", "#login", function() {
-		$("#modal__login").fadeIn(300),
-		$("body").css("overflow", "hidden")
-	}),
+		// ВходРегистрация
+		$("body").on("click", "#registration", function () {
+			$("#modal__reg").fadeIn(300),
+				$("body").css("overflow", "hidden")
+		}),
+		// Вход
+		$("body").on("click", "#login", function () {
+			$("#modal__login").fadeIn(300),
+				$("body").css("overflow", "hidden")
+		}),
 
-	// Переход с регистации на логин и обратно
-	$("body").on("click", "#open__modal__login", function() {
-		$("#modal__login").fadeIn(300),
-		$("#modal__reg").fadeOut(300),
-		$("body").css("overflow", "hidden")
-	}),
-	$("body").on("click", "#open__modal__reg", function() {
-		$("#modal__reg").fadeIn(300),
-		$("#modal__login").fadeOut(300),
-		$("body").css("overflow", "hidden")
-	}),
+		// Переход с регистации на логин и обратно
+		$("body").on("click", "#open__modal__login", function () {
+			$("#modal__login").fadeIn(300),
+				$("#modal__reg").fadeOut(300),
+				$("body").css("overflow", "hidden")
+		}),
+		$("body").on("click", "#open__modal__reg", function () {
+			$("#modal__reg").fadeIn(300),
+				$("#modal__login").fadeOut(300),
+				$("body").css("overflow", "hidden")
+		}),
 
 
-	// Закрытие модальных окон
-	$(".modal__feedback__close").click(function() {
-		$("#modal__feedback").fadeOut(300),
-		$("body").css("overflow", "auto")
-	}),
-	// $("body").on("click", ".header__profile__login", function() {
-	// 	$("#modal__reg").fadeIn(300),
-	// 	$("body").css("overflow", "hidden")
-	// }),
-	$(".modal__feedback__close").click(function() {
-		$("#modal__reg").fadeOut(300),
-		$("body").css("overflow", "auto")
-	}),
-	$(".modal__feedback__close").click(function() {
-		$("#modal__reg").fadeOut(300),
-		$("body").css("overflow", "auto")
-	}),
-	$(".modal__feedback__close").click(function() {
-		$("#modal__login").fadeOut(300),
-		$("body").css("overflow", "auto")
-	});
+		// Закрытие модальных окон
+		$(".modal__feedback__close").click(function () {
+			$("#modal__feedback").fadeOut(300),
+				$("body").css("overflow", "auto")
+		}),
+		// $("body").on("click", ".header__profile__login", function() {
+		// 	$("#modal__reg").fadeIn(300),
+		// 	$("body").css("overflow", "hidden")
+		// }),
+		$(".modal__feedback__close").click(function () {
+			$("#modal__reg").fadeOut(300),
+				$("body").css("overflow", "auto")
+		}),
+		$(".modal__feedback__close").click(function () {
+			$("#modal__reg").fadeOut(300),
+				$("body").css("overflow", "auto")
+		}),
+		$(".modal__feedback__close").click(function () {
+			$("#modal__login").fadeOut(300),
+				$("body").css("overflow", "auto")
+		});
 });
 var login = $('.header__profile__login');
-login.hover(function(){
+login.hover(function () {
 	$('.but__personal').addClass('active__but__personal')
 }),
-login.mouseleave(function(){
-	setTimeout(function(){
+	login.mouseleave(function () {
+		setTimeout(function () {
+			$('.but__personal').removeClass('active__but__personal')
+		}, 1500)
+	}),
+
+	login.click(function () {
+		$('.but__personal').toggleClass('active__but__personal')
+	}),
+	$('.but__personal span').click(function () {
 		$('.but__personal').removeClass('active__but__personal')
-	}, 1500)
-}),
+	}),
 
-login.click(function(){
-	$('.but__personal').toggleClass('active__but__personal')
-}),
-$('.but__personal span').click(function(){
-	$('.but__personal').removeClass('active__but__personal')
-}),
+	// Mask Phone JS
+	window.addEventListener("DOMContentLoaded", function () {
+		var keyCode;
 
-// Mask Phone JS
-window.addEventListener("DOMContentLoaded", function() {
-	var keyCode;
-	
-	function mask(event) {
-	event.keyCode && (keyCode = event.keyCode);
-	var pos = this.selectionStart;
-	if (pos < 1) event.preventDefault();
-	var matrix = "+7 (___) ___-__-__",
-	i = 0,
-	def = matrix.replace(/\D/g, ""),
-	val = this.value.replace(/\D/g, ""),
-	new_value = matrix.replace(/[_\d]/g, function(a) {
-	return i < val.length? val.charAt(i++) || def.charAt(i): a
+		function mask(event) {
+			event.keyCode && (keyCode = event.keyCode);
+			var pos = this.selectionStart;
+			if (pos < 1) event.preventDefault();
+			var matrix = "+7 (___) ___-__-__",
+				i = 0,
+				def = matrix.replace(/\D/g, ""),
+				val = this.value.replace(/\D/g, ""),
+				new_value = matrix.replace(/[_\d]/g, function (a) {
+					return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+				});
+			i = new_value.indexOf("_");
+			if (i != -1) {
+				i < 3 && (i = 1);
+				new_value = new_value.slice(0, i)
+			}
+			var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+				function (a) {
+					return "\\d{1," + a.length + "}"
+				}).replace(/[+()]/g, "\\$&");
+			reg = new RegExp("^" + reg + "$");
+			if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+			if (event.type == "blur" && this.value.length < 5) this.value = ""
+		}
+		var input = document.querySelector("#tel");
+		input.addEventListener("input", mask, false);
+		input.addEventListener("focus", mask, false);
+		input.addEventListener("blur", mask, false);
+		input.addEventListener("keydown", mask, false)
 	});
-	i = new_value.indexOf("_");
-	if (i != -1) {
-	i < 3 && (i = 1);
-	new_value = new_value.slice(0, i)
-	}
-	var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-	function(a) {
-	return "\\d{1," + a.length + "}"
-	}).replace(/[+()]/g, "\\$&");
-	reg = new RegExp("^" + reg + "$");
-	if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-	if (event.type == "blur" && this.value.length < 5) this.value = ""
-	}
-	var input = document.querySelector("#tel");
-	input.addEventListener("input", mask, false);
-	input.addEventListener("focus", mask, false);
-	input.addEventListener("blur", mask, false);
-	input.addEventListener("keydown", mask, false)
-});
 // FooterDown
-(function(){
+(function () {
 	footer();
-	$(window).resize(function() {
+	$(window).resize(function () {
 		footer();
 	});
 	function footer() {
@@ -268,12 +268,31 @@ window.addEventListener("DOMContentLoaded", function() {
 		}
 	}
 })();
-$(function() {
+$(function () {
 	var out = $('a').not('#gallary a');
-	out.click(function(e){
+	out.click(function (e) {
 		e.preventDefault();
 		$('body').addClass('out');
 		var t = $(this);
-		setTimeout(function(){ location.href = t.attr('href'); }, 300);
+		setTimeout(function () { location.href = t.attr('href'); }, 300);
 	});
 });
+
+function input_val(dir, elid) {
+	//console.log(dir + " " + elid)
+	var inputEl = document.getElementById(elid)
+	//console.log(inputEl)
+	var value = parseInt(inputEl.value, 10)
+	if (isNaN(value))
+		value = 0
+	if (dir == "dec")
+		value--
+	else if (dir == "inc")
+		value++
+	else if (dir <= "0")
+		return;
+	inputEl.value = value
+	$("#calc__product__number").keyup(function(e){
+		this.value = this.value.replace(/[^0-9\.]/g, '');
+	  });
+}
