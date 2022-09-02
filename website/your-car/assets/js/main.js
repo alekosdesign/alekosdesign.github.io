1fafeec5ff831cd60ea9947507f87e7391b82bbd
AlEkos Design <38691899+alekosdesign@users.noreply.github.com>
2022-09-02 16:22:08 +1000

    addfix

--- website/your-car/assets/js/main.js
+++ website/your-car/assets/js/main.js
@@ -1,61 +1,43 @@
-// // Preloader
-// document.body.onload = function(){
-// 	setTimeout(function() {
-// 		var preloader = document.getElementById('page-preloader');
-// 		if(!preloader.classList.contains('done') )
-// 		{
-// 			preloader.classList.add('done');
-// 		}
-// 	}, 500);
-// };
-// // Mainmenu Scroll Min
-// $(window).scroll(function(){
-// 	var height = $(window).scrollTop();
-// 	if (height > 50){
-// 		$('header').addClass('scroll');
-// 	}else{
-// 		$('header').removeClass('scroll');
-// 	}
-// });
-// $(document).ready(function() {
-// 	var $page = $('html, body');
-// 	$('a[href*="#"]').click(function() {
-// 		$page.animate({
-// 			scrollTop: $($.attr(this, 'href')).offset().top
-// 		}, 1000);
-// 		return false;
-// 	});
-// });
-// $('.mainmenu').click("click touchstart", function(){
-// 	$('nav').toggleClass("active__mainmenu");
-// 	$('section, .hero').toggleClass("blur");
-// 	$('.mainmenu').toggleClass("close");
-// });
-// // Parallax
-// $(document).ready(function() {
-// 	$(window).scroll(function(){
-// 		var sl = $(window).scrollTop();
-// 		if (sl>0) {
-// 			$('.hero .hero__bg__smoke').css('margin-top',sl/2 +'px')
-// 			$('.hero .hero__bg__smoke__2').css('margin-top',sl +'px')
-// 		}
-// 	})
-// });
+// Preloader
+document.body.onload = function(){
+	setTimeout(function() {
+		var preloader = document.getElementById('page-preloader');
+		if(!preloader.classList.contains('done') )
+		{
+			preloader.classList.add('done');
+			setTimeout(function(){
+				jQuery('#page-preloader').css({ 'display': 'none' });
+			}, 300);
+		}
+	}, 500);
+};
 
+jQuery(document).ready(function() {
+    jQuery(window).width(),
+    jQuery(".hero").mousemove(function(e) {
+        var xpage = .01 * (jQuery(window).width() / 2 - e.pageX)
+          , ypage = .02 * (jQuery(window).width() / 2 - e.pageY);
+		  jQuery(".car__hero").css("transform", "translate(" + xpage + "px, " + ypage + "px)")
+    })
+});
+jQuery(document).ready(function () {
+	var h = jQuery('footer').outerHeight();
+	jQuery('.wrap__footer').css({ 'height': h });
+});
 // Mainmenu Scroll Min
-$(window).scroll(function(){
-	var height = $(window).scrollTop();
-	if (height>500){
-		$('.header').addClass('scroll');
+jQuery(window).scroll(function(){
+	var height = jQuery(window).scrollTop();
+	if (height>250){
+		jQuery('.header').addClass('scroll');
 	}else{
-		$('.header').removeClass('scroll');
+		jQuery('.header').removeClass('scroll');
 	}
 });
-$(document).ready(function() {
-	var $page = $('html, body');
-	$('a[href*="#"]').click(function() {
-		$page.animate({
-			scrollTop: $($.attr(this, 'href')).offset().top
+jQuery(document).ready(function() {
+	var jQuerypage = jQuery('html, body');
+	jQuery('a[href*="#"]').click(function() {
+		jQuerypage.animate({
+			scrollTop: jQuery(jQuery.attr(this, 'href')).offset().top
 		}, 1000);
 		return false;
 	});
